8dc4744504bf0fafecd604d72a4ac2128eafd49c
enocklubowa <lubowaenock34@gmail.com>
2021-01-06 22:26:17 +0300

    --Cost of Tile finished

---
+++ Numbers/CostOfTile.ws.kts
@@ -0,0 +1,22 @@
+
+println(totalCost(2000.0, Tile(2.0), 8.0, 6.0))
+
+fun tilesRequiredForWidth(tile: Tile, floorWidth: Double): Int{
+    return (floorWidth/tile.length).toInt()
+}
+
+fun tilesRequiredForLength(tile: Tile, floorLength: Double): Int{
+    return (floorLength/tile.length).toInt()
+}
+
+fun totalTilesRequired(tile: Tile, floorLength: Double, floorWidth: Double): Int{
+    return tilesRequiredForLength(tile, floorLength)*tilesRequiredForWidth(tile, floorWidth)
+}
+
+fun totalCost(tilePrice: Double, tile: Tile, floorLength: Double, floorWidth: Double): Double{
+    return (totalTilesRequired(tile, floorLength, floorWidth).toDouble())*tilePrice
+}
+
+class Tile(var length: Double){
+
+}
